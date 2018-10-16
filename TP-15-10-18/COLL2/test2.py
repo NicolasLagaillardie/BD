3a9f6baa9803085fb3bdedc7932cc5de62205bc6
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-10-16 20:43:03 +0200

    Test same ID

--- TP-15-10-18/COLL2/test2.py
+++ TP-15-10-18/COLL2/test2.py
@@ -6,112 +6,153 @@ Created on Mon Oct 15 08:34:08 2018
 @author: lag
 """
 
+"""Import for spliting"""
 import re
 
-linksList = []
-maxi = 0
-
-with open("en.wikipedia.org/enwiki-20110405-CategoryIdGraph.txt") as infile:
-    for line in infile:
-        
-        elt = re.split(r'\t+', line.rstrip('\t'))
-        subElt = re.split(r'\n+', elt[1].rstrip('\n'))
-        
-        linksList.append((int(elt[0]),int(subElt[0])))
-        
-        maxi = max(maxi,int(elt[0]),int(subElt[0]))
-
-URLList = []
-
-indexList = [-1] * (1 + maxi)
-
-vertices = []
-
-indexElt = 0
-        
-with open("en.wikipedia.org/enwiki-20110405-CategoryIdName.txt") as infile:
-    for line in infile:
-        
-        elt = re.split(r'\t+', line.rstrip('\t'))
-        subElt = re.split(r'\n+', elt[1].rstrip('\n'))
-        
-        URLList.append(subElt[0])
-        
-        indexList[int(elt[0])] = indexElt
-        
-        vertices.append(int(elt[0]))
-        
-        indexElt += 1
-
-print(URLList[:10])
-
-adjacentList = [[] for i in range(indexElt + 1) ]
-
-for i in linksList :
-    adjacentList[indexList[i[0]]].append(i[1])
-    
-print(adjacentList[0])
-
-"""Algo Tarjan"""
-
-verticeIndex = [-1 for i in range(indexElt + 1) ]
-verticeLowLink = [-1 for i in range(indexElt + 1) ]
-verticeOnStack = [False for i in range(indexElt + 1) ]
-
 index = 0
-S = []
+    
+def main(filePath1, filePath2) :
+    """ "en.wikipedia.org/enwiki-20110405-CategoryIdGraph.txt" / "en.wikipedia.org/enwiki-20110405-CategoryIdName.txt" """
+    
+    """linksList contains  """
+    linksList = []
+    maxi = 0
+    
+    with open(filePath1) as infile:
+        for line in infile:
+            
+            elt = re.split(r'\t+', line.rstrip('\t'))
+            subElt = re.split(r'\n+', elt[1].rstrip('\n'))
+            
+            linksList.append((int(elt[0]),int(subElt[0])))
+            
+            maxi = max(maxi,int(elt[0]),int(subElt[0]))
+    
+    URLList = []
+    
+    indexList = [-1] * (1 + maxi)
+    
+    vertices = []
+    
+    indexElt = 0
+            
+    with open(filePath2) as infile:
+        for line in infile:
+            
+            elt = re.split(r'\t+', line.rstrip('\t'))
+            subElt = re.split(r'\n+', elt[1].rstrip('\n'))
+            
+            URLList.append(subElt[0])
+            
+            indexList[int(elt[0])] = indexElt
+            
+            vertices.append(int(elt[0]))
+            
+            indexElt += 1
+    
+    print(URLList[:10])
+    
+    adjacentList = [[] for i in range(indexElt + 1) ]
+    
+    for i in linksList :
+        adjacentList[indexList[i[0]]].append(i[1])
+        
+    print(adjacentList[0])
 
-
-def strongconnect(vertice) :
-    """wait for int"""
-    """Set the depth index for v to the smallest unused index"""
+    """Algo Tarjan"""
     
-    output = []
-
-    global index
-
-    tempIndex = indexList[vertice]
+    verticeIndex = [-1 for i in range(indexElt + 1) ]
+    verticeLowLink = [-1 for i in range(indexElt + 1) ]
+    verticeOnStack = [False for i in range(indexElt + 1) ]
     
-    verticeIndex[tempIndex] = index
-    verticeLowLink[tempIndex] = index
+    S = []
     
-    index += 1
     
-    S.append(vertice)
+    def strongconnect(vertice) :
+        """wait for int"""
+        """Set the depth index for v to the smallest unused index"""
+        
+        output = []
     
-    verticeOnStack[tempIndex] = True
+        global index
     
-    """Consider successors of v"""
-    for i in adjacentList[tempIndex] :
-        tempNeighbour = indexList[i]
-        if (verticeIndex[tempNeighbour] == -1):
-            strongconnect(i)
-        elif (verticeOnStack[tempNeighbour]) :
-            """Successor w is in stack S and hence in the current SCC
-            If w is not on stack, then (v, w) is a cross-edge in the DFS tree and must be ignored
-            Note: The next line may look odd - but is correct.
-            It says w.index not w.lowlink; that is deliberate and from the original paper"""
-            verticeLowLink[tempIndex] = min(verticeLowLink[tempIndex], verticeIndex[tempNeighbour])
-    
-    if (verticeLowLink[tempIndex] == verticeIndex[tempIndex]) :
-        w = S.pop()
-        verticeLowLink[indexList[w]]
-        output.append(w)
-        while (vertice != w) :
+        tempIndex = indexList[vertice]
+        
+        verticeIndex[tempIndex] = index
+        verticeLowLink[tempIndex] = index
+        
+        index += 1
+        
+        S.append(vertice)
+        
+        verticeOnStack[tempIndex] = True
+        
+        """Consider successors of v"""
+        for i in adjacentList[tempIndex] :
+            tempNeighbour = indexList[i]
+            if (verticeIndex[tempNeighbour] == -1):
+                strongconnect(i)
+            elif (verticeOnStack[tempNeighbour]) :
+                """Successor w is in stack S and hence in the current SCC
+                If w is not on stack, then (v, w) is a cross-edge in the DFS tree and must be ignored
+                Note: The next line may look odd - but is correct.
+                It says w.index not w.lowlink; that is deliberate and from the original paper"""
+                verticeLowLink[tempIndex] = min(verticeLowLink[tempIndex], verticeIndex[tempNeighbour])
+        
+        if (verticeLowLink[tempIndex] == verticeIndex[tempIndex]) :
             w = S.pop()
             verticeLowLink[indexList[w]]
             output.append(w)
-        return output
-            
-outputList = []
-            
-URLOutputList = []
+            while (vertice != w) :
+                w = S.pop()
+                verticeLowLink[indexList[w]]
+                output.append(w)
+            return output
+                
+    outputList = []
+                
+    URLOutputList = []
+    
+    for vertice in vertices :
+        if (verticeIndex[indexList[vertice]] == -1) :
+            temp = strongconnect(vertice)
+            if(temp):
+                outputList.append(temp)
+                URLOutputList.append([])
+                for elt in temp:
+                    URLOutputList[-1].append(URLList[indexList[elt]])
+                    
+    return [outputList, URLOutputList]
 
-for vertice in vertices :
-    if (verticeIndex[indexList[vertice]] == -1) :
-        temp = strongconnect(vertice)
-        if(temp):
-            outputList.append(temp)
-            URLOutputList.append([])
-            for elt in temp:
-                URLOutputList[-1].append(URLList[indexList[elt]])
+def testSame(filePath1, filePath2) :
+    
+    linesFile1 = []
+    linesFile2 = []
+    
+    with open(filePath1) as infile:
+        for line in infile:
+            
+            elt = re.split(r'\t+', line.rstrip('\t'))
+            
+            linesFile1.append(int(elt[0]))
+            
+    with open(filePath2) as infile:
+        for line in infile:
+            
+            elt = re.split(r'\t+', line.rstrip('\t'))
+            
+            linesFile2.append(int(elt[0]))
+            
+    for i in linesFile1:
+        if (i in linesFile2):
+            return True
+        
+    return False
+
+""" Check if there are elements with same ID """
+#print(testSame("en.wikipedia.org/enwiki-20110405-CategoryIdName.txt", "es.wikipedia.org/eswiki-20110420-CategoryIdName.txt"))
+
+result = main("en.wikipedia.org/enwiki-20110405-CategoryIdGraph.txt", "en.wikipedia.org/enwiki-20110405-CategoryIdName.txt")
+
+#print(result[0])
+#print(result[1])
